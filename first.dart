dac96773b1023e00f50f4b1724a7de9808477b7a
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-10-15 22:29:26 -0700

    Hello world
    A Simple Hello World program which often many programmers used their code for the first time. Dart Language uses the main function to execute the statements .Also, in this code single and multi-line comments become clear

---
+++ first.dart
@@ -0,0 +1,12 @@
+void main() {
+  print("hello world");
+  //for example when you want to write the integers value in print statement then no need to write in double quotation
+  print(89);
+  print(8 + 9);
+  //no need to wite the \n for the next line
+  print(true);
+}
+/*this is themulti line 
+comment in dart */
+///this is our documntation comment generally used foe referencing the comments it 
+///can be singleline line comment and multi line 
