16049aeca63d0ed9c527773391f2a8623f0a7409
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-10-17 08:39:11 -0700

    Constants In DART
    Constants concept is very essential concept in DART. Const and Final are the keywords for declaring the constant.

---
+++ constant.dart
@@ -0,0 +1,12 @@
+void main() {
+  //constant is the term in which we nevers change the value
+  //Constant can be used in two ways
+  // 1) Final keyword
+  // 2) const "keyword (it cannot used directly inside the class rather we used const static keyword for the claasss)
+  //const name = "Babar ";
+  //const int age = 19;
+  //we cannot change the value of constant
+  final semester = 8;
+  final double CGPA = 4.0;
+  print("${semester}   " + "   ${CGPA}");
+}
