79b6ff157ccfedf46092fd01e57070f25b2e4d3b
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-10-19 11:28:42 -0700

    Variables in Dart
    Simple Code for Creating the Variables in DART.

---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main() {
+  //variables in dart can be declared in three ways i-e type,type checking and dynamic
+  var name = "kashif";
+  var age = 18;
+  print(name);
+  print(age);
+  //this is type syntax  in the dart which tells the compiler in run time which type the variable hold
+  String name1 = "Noman";
+  int age2 = 19;
+  //this is type checking which is same as the c/c++
+  dynamic name3 = "Samay";
+  dynamic x = 10;
+  //this the dynamic time which uses the dynamic keyword for declaring the variable
+  //boolean data type
+  bool isActive = true;
+  print(isActive);
+}
