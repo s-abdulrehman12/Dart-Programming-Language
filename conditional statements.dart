f16b864abb9551445a5018c9fb6b3b7deccdb10b
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-10-21 04:46:52 -0700

    Conditional Statements
    Program to show the conditional statements in the DART Programming Language

---
+++ conditional statements.dart
@@ -0,0 +1,23 @@
+void main() {
+  //if statement only
+  var num = 10;
+  if (num > 0) {
+    print("The Number is Positive :");
+  }
+  //if else statement only
+  var no = -7;
+  if (no > 0) {
+    print("The number is positive ");
+  } else {
+    print("The number is negative :");
+  }
+  //else if ladder statement
+  var signal = "green";
+  if (signal == "red") {
+    print("Stop :");
+  } else if (signal == "yellow") {
+    print("Ready to go :");
+  } else {
+    print("Go :)");
+  }
+}
